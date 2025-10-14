2c51342a1eec636c3210cf617df88f28646f2c2f
Elias Lackner <lackner.elias@gmail.com>
2025-10-14 09:38:37 +0200

    fix: ignore id filter in MultiFilter

--- tests/Rule/fixtures/NoDALFilterByID/criteria.php
+++ tests/Rule/fixtures/NoDALFilterByID/criteria.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\NotFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\MultiFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\NotFilter;
 
 $criteria = new Criteria();
 $criteria->addFilter(new EqualsFilter('id', '12345'));
+
+// This should be allowed when wrapped in MultiFilter
+$criteria->addFilter(new MultiFilter(
+    MultiFilter::CONNECTION_OR,
+    [
+        new EqualsFilter('id', '12345'),
+    ],
+));
 
 // This should be allowed when wrapped in NotFilter
 $criteria->addFilter(new NotFilter(
